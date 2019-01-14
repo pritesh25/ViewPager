69eb0a2e33d62c35347191bf38e043f1093440c5
pritesh <priteshvishwakarma@gmail.com>
2019-01-14 11:12:40 +0530

    2 diff project for backpress

---
+++ FragmentBackPress/src/main/java/com/herba/sdk/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package com.herba.sdk.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.herba.sdk.myapplication.backstack.FirstFragment;
+import com.herba.sdk.myapplication.backstack.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements FirstFragment.FirstFragmentCallback,SecondFragment.SecondFragmentCallback {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        replaceFragment(new FirstFragment());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.d(TAG,"fragment count = "+getSupportFragmentManager().getBackStackEntryCount());
+
+        if(getSupportFragmentManager().getBackStackEntryCount() == 1)
+        {
+            finish();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment (Fragment fragment) {
+        String backStateName = fragment.getClass().getName();
+
+        FragmentManager manager = getSupportFragmentManager();
+        boolean fragmentPopped = manager.popBackStackImmediate(backStateName, 0);
+
+        if (!fragmentPopped) { //fragment not in back stack, create it.
+            FragmentTransaction ft = manager.beginTransaction();
+            ft.replace(R.id.frameLayout, fragment);
+            ft.addToBackStack(backStateName);
+            ft.commit();
+        }
+    }
+
+    @Override
+    public void goToSecond() {
+        replaceFragment(new SecondFragment());
+    }
+
+    @Override
+    public void goToFirst() {
+        replaceFragment(new FirstFragment());
+    }
+
+}
---
+++ FragmentBackPress/src/main/java/com/herba/sdk/myapplication/backstack/SecondFragment.java
@@ -0,0 +1,66 @@
+package com.herba.sdk.myapplication.backstack;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.herba.sdk.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    public SecondFragmentCallback secondFragmentCallback;
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        secondFragmentCallback = (SecondFragmentCallback)context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(secondFragmentCallback != null)
+                {
+                    secondFragmentCallback.goToFirst();
+                }
+                else
+                {
+                    Log.d(TAG,"null callkback");
+                }
+            }
+        });
+
+    }
+
+    public interface SecondFragmentCallback {
+        void goToFirst();
+    }
+}
